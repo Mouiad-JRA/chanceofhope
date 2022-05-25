486fc7acd9206aa0b659994411046fc0681c735f
Mouiad JRA <mouiad.alilo@gmail.com>
2022-05-25 17:20:06 +0300

    WIP : Fix static path #4
    - add static and media settings in main urls

--- chanceofhope/urls.py
+++ chanceofhope/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
 ]
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
